1c8eef7b4a0e6c5aa9d94d5239fcd79a7834509a
Radoya aftab <raimaaftab73@gmail.com>
2024-10-20 20:56:12 +0600

    first commit

---
+++ operator.py
@@ -0,0 +1,25 @@
+class A:
+
+    def __init__(self,a):
+        self.a = a
+
+    def __lt__(self,other):
+        if(self.a < other.a):
+            return "ob1 is less then ob2"
+        else:
+            return "ob2 is less than ob1"
+
+    def __eq__(self,other):
+        if(self.a == other.a):
+            return "both are equal"
+        else:
+            return "not equal"
+        
+ob1 = A(2)
+ob2 = A(3)
+print("passed values :",ob1.a,ob2.a)
+print(ob1 < ob2)
+ob3 = A(4)
+ob4 = A(4)
+print("passed values :",ob3.a,ob4.a)
+print(ob3 == ob4)
